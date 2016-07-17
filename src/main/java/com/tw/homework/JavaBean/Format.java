acb3818f0d5bdd83e43952b9c70a42725a227b06
Yasic <2207793765@qq.com>
2016-07-17 15:56:24 +0800

    Update base strategy class to interface

---
+++ src/main/java/com/tw/homework/JavaBean/Format.java
@@ -0,0 +1,13 @@
+package com.tw.homework.JavaBean;
+
+/**
+ * Created by Yasic on 2016/7/17.
+ */
+public class Format {
+    private String nameScope;
+    private String numberScope;
+    private float priceScope;
+    private float totalMoneyScope;
+    private float saveMoneyScope;
+
+}
